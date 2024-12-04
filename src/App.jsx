5a3662933d9b6ca1588f9c26702c6eec9d15d4ae
kaktusbezcherepa <eli1starboom@gmail.com>
2024-12-05 01:09:30 +0300

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import ClientsTable from "./components/Table"
+import { Box } from "@mui/material";
+import { createClient } from "../services/clientsService";
+import {Modal, Button} from "react-bootstrap";
+import ClientForm from "./components/CreationForm";
 
 function ClientList() {
   // const [clients, setClients] = useState([]);
   // const [error, setError] = useState(null);
-  // const [show, setShow] = useState(false);
-  // const [isLoading, setIsLoading] = useState(true)
+  const [show, setShow] = useState(false);
   
   
   // const fetchClients = async () => {
@@ -25,91 +28,35 @@ function ClientList() {
   //   return <div>Error: {error}</div>;
   // }
 
-  // function handleClose() {
-  //   setShow(false);
-  //   fetchClients();
-  // }
+  function handleClose() {
+    setShow(false);
+  }
 
-  // function handleShow() {
-  //   setShow(true);
-  // }
+  function handleShow() {
+    setShow(true);
+  }
 
-  // function handleDelete(id) {
-  //   deleteClient(id)
-  //     .then(() => {
-  //       setClients((prevClients) =>
-  //         prevClients.filter((client) => client.id !== id)
-  //       );
-  //       console.log("Client successfully deleted");
-  //     })
-  //     .catch((err) => {
-  //       setError(err.response?.data?.message || "Failed to delete client");
-  //     });
-  //   fetchClients();
-  //   console.log("Client successfully deleted");
-  // }
 
-  // function handleCreate(data) {
-  //   createClient(data);
-  // }
+  function handleCreate(data) {
+    createClient(data);
+  }
 
   return (
+    <div>
     <ClientsTable />
+    <div className="d-flex justify-content-center">
+    <Button onClick={handleShow}>Добавить клиента</Button>
 
-    // <div>
-    //   <Container>
-    //     <Table striped bordered hover>
-    //       <thead>
-    //         <tr>
-    //           <th>ID</th>
-    //           <th>Фамилия Имя Отчество</th>
-    //           <th>Дата и время создания</th>
-    //           <th>Последние изменения</th>
-    //           <th>Контакты</th>
-    //           <th>Действия</th>
-    //         </tr>
-    //       </thead>
-    //       <tbody>
-    //         {clients.map((client) => (
-    //           <tr key={client.id}>
-    //             <td>{client.id}</td>
-    //             <td>{`${client.name} ${client.surname} ${
-    //               client.lastName || ""
-    //             }`}</td>
-    //             <td>{`${extractDateTime(client.createdAt).date} ${
-    //               extractDateTime(client.createdAt).time
-    //             }`}</td>
-    //             <td>{`${extractDateTime(client.updatedAt).date} ${
-    //               extractDateTime(client.updatedAt).time
-    //             }`}</td>
-    //             <td>{client.contacts}</td>
-    //             <td className="d-inline-flex p-2">
-    //               <Button variant="info">Изменить</Button>
-    //               <Button
-    //                 variant="danger"
-    //                 onClick={() => handleDelete(client.id)}
-    //               >
-    //                 Удалить
-    //               </Button>
-    //             </td>
-    //           </tr>
-    //         ))}
-    //       </tbody>
-    //     </Table>
-    //   </Container>
-    //   <div className="d-flex justify-content-center">
-    //     <Button onClick={handleShow}>Добавить клиента</Button>
-
-    //     <Modal show={show} onHide={handleClose} size="lg">
-    //       <Modal.Header closeButton>
-    //         <Modal.Title>Добавление клиента</Modal.Title>
-    //       </Modal.Header>
-    //       <Modal.Body>
-    //         <ClientForm onClose={handleClose} />
-    //       </Modal.Body>
-    //     </Modal>
-    //   </div>
-    // </div>
+    <Modal show={show} onHide={handleClose} size="lg">
+      <Modal.Header closeButton>
+        <Modal.Title>Добавление клиента</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <ClientForm onClose={handleClose} />
+      </Modal.Body>
+    </Modal>
+  </div>
+  </div>
   );
 }
 
