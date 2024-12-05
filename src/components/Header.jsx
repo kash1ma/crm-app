b963c89d87dd099559f715620f37cea1c88f0f8f
Artem <artval570@gmail.com>
2024-12-05 13:25:42 +0300

    addImg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,26 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import logo from '../assets/logo.webp'
 
 function Header() {
   return (
     <AppBar sx={{ bgcolor: "#282828" }}>
       <Toolbar>
-        <Box sx={{ width: '50%' }}>
+        <Box sx={{ display: "flex", alignItems: "center", width: "50%" }}>
+          <Box sx={{ mr: 5 }}>
+            <img 
+              src={logo}
+              alt="icon" 
+              style={{ height: "40px", width: "40px" }} 
+            />
+          </Box>
           <TextField
             variant="outlined"
             placeholder="Введите запрос"
             size="small"
             InputProps={{
-              style: {
+              sx: {
                 bgcolor: "#3a3a3a",
                 color: "#fff",
                 borderRadius: "4px",
