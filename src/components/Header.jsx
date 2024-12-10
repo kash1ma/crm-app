7437d9694843e2f0c80668fef400f59607419da5
kash1ma <ayollo.matvey@gmail.com>
2024-12-11 00:11:45 +0300

    Working search, rn logs client on click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import logo from "../assets/logogo.png";
+import { getClients } from "../../services/clientsService.js";
 
 function Header() {
+  const [clients, setClients] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredClients, setFilteredClients] = useState([]);
+
+  const fetchClients = async () => {
+    try {
+      const data = await getClients();
+      setClients(data);
+      console.log(data);
+    } catch (err) {
+      setError(err.response?.data?.message || "Failed to fetch clients");
+    }
+  };
+
+  useEffect(() => {
+    fetchClients();
+  }, []);
+
+  const handleSearch = (event, value) => {
+    setSearchTerm(value);
+    //const fullName = client.name + " " + client.surname;
+    if (value) {
+      const filtered = clients.filter((client) => {
+        const fullName = client.name + " " + client.surname;
+        return fullName.toLowerCase().includes(value.toLowerCase());
+      });
+      setFilteredClients(filtered);
+    } else {
+      setFilteredClients([]);
+    }
+  };
+
+  const handleClientSelect = (event, client) => {
+    if (client && client.id) {
+      console.log("Selected client ID:", client.id);
+    }
+  };
+
   return (
-    <AppBar sx={{ bgcolor: "#282828"}}>
+    <AppBar sx={{ bgcolor: "#282828" }}>
       <Toolbar>
         <Box sx={{ display: "flex", alignItems: "center", width: "50%" }}>
           <Box sx={{ mr: 5 }}>
@@ -17,17 +57,30 @@ function Header() {
               style={{ height: "45px", width: "45px" }}
             />
           </Box>
-          <TextField
-            variant="outlined"
-            placeholder="Введите запрос"
-            size="small"
-            InputProps={{
-              sx: {
-                bgcolor: "#3a3a3a",
-                color: "#fff",
-                borderRadius: "4px",
-              },
-            }}
+          <Autocomplete
+            freeSolo
+            options={filteredClients}
+            getOptionLabel={(option) =>
+              option.name + " " + option.surname || ""
+            }
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="outlined"
+                placeholder="Введите запрос"
+                size="small"
+                InputProps={{
+                  ...params.InputProps,
+                  sx: {
+                    bgcolor: "#3a3a3a",
+                    color: "#fff",
+                    borderRadius: "4px",
+                  },
+                }}
+              />
+            )}
+            onInputChange={handleSearch}
+            onChange={handleClientSelect}
             fullWidth
           />
         </Box>
