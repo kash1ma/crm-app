feb033ef31ef210525fbab2e3831384ebaaecf85
kaktusbezcherepa <eli1starboom@gmail.com>
2024-12-15 17:39:19 +0300

    ch

--- src/UserProfile.jsx
+++ src/UserProfile.jsx
@@ -44,6 +44,7 @@ export default function UserProfile() {
       try {
         const userData = await getClientById(id);
         setClient(userData);
+        console.log(client);
       } catch (error) {
         console.error("Error fetching user data:", error);
       }
@@ -55,8 +56,6 @@ export default function UserProfile() {
   if (!client) {
     return <div>Loading...</div>;
   }
-
-  console.log(client);
 
   function handleDelete() {
     setClientToDelete(client.id);
@@ -124,301 +123,350 @@ export default function UserProfile() {
     }
   };
 
+  console.log(client);
+
+  const handleInputChange = (e, value) => {
+    if (e === "surname") {
+      setClient((prev) => ({
+        ...prev,
+        [e]: value,
+      }));
+    }
+    if (e === "name") {
+      setClient((prev) => ({
+        ...prev,
+        [e]: value,
+      }));
+    }
+    if (e === "lastName") {
+      setClient((prev) => ({
+        ...prev,
+        [e]: value,
+      }));
+    }
+    if(e === "phone"){
+      console.log(client.contacts[1].value)
+      setClient((prev) => ({
+        ...prev,
+        [client.contacts[1].value]: value,
+      }));
+    }
+  };
+
   function handleReturn() {
     navigate("/");
   }
 
   return (
-    <Container
-      sx={{
-        color: "#fff",
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-      maxWidth="xxl"
-    >
-      <Box
-        sx={{
-          width: "100%",
-          maxWidth: "1500px", // Adjust the maxWidth as needed
-          padding: 4,
-        }}
+    <>
+      <Button
+        onClick={handleReturn}
+        sx={{ color: "white", position: "absolute", top: 30, left: 30 }}
       >
-
-        <Button
-          onClick={handleReturn}
-          sx={{ color: "white", position: "relative", right: "4rem" }}
+        <FirstPageIcon sx={{ width: "2rem", height: "2rem" }}></FirstPageIcon>
+      </Button>
+      <Container
+        sx={{
+          color: "#fff",
+          minHeight: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+        maxWidth="xxl"
+      >
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: "1500px", // Adjust the maxWidth as needed
+            padding: 4,
+          }}
         >
-          <FirstPageIcon sx={{ width: "2rem", height: "2rem" }}></FirstPageIcon>
-        </Button>
-        <Typography variant="h1" sx={{ fontWeight: "bolder", mb: 4 }}>
-          {`${client.name} ${client.surname} ${client.lastName || ""} `} ID{id}
-        </Typography>
-
-        <Grid2 container spacing={29}>
-          <Grid2
-            item
-            xs={12}
-            md={6}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 5,
-              paddingLeft: "3rem",
-              paddingRight: "3rem",
-            }}
+          <Typography
+            variant="h1"
+            sx={{ fontWeight: "bolder", mb: 4, fontSize: "5vh", textAlign: "center" }}
           >
-            <Typography
-              variant="h5"
-              sx={{ mb: 2, fontSize: 50, margin: "0 auto", fontWeight: "bold" }}
-            >
-              User Info
-            </Typography>
-            <Box
+            {`${client.name} ${client.surname} ${client.lastName || ""} `} ID
+            {id}
+          </Typography>
+
+          <Grid2 container spacing={29}>
+            <Grid2
+              item
+              xs={12}
+              md={6}
               sx={{
                 display: "flex",
                 flexDirection: "column",
                 gap: 5,
-                color: "white",
+                paddingLeft: "3rem",
+                paddingRight: "3rem",
               }}
             >
-              <TextField
-                label="Фамилия"
-                value={client.lastName || ""}
-                onChange={(e) => handleInputChange("lastName", e.target.value)}
-                sx={style}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-              />
-
-              <TextField
-                label="Имя"
-                value={client.firstName || ""}
-                onChange={(e) => handleInputChange("firstName", e.target.value)}
-                sx={style}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-              />
-
-              <TextField
-                label="Отчество"
-                value={client.middleName || ""}
-                onChange={(e) =>
-                  handleInputChange("middleName", e.target.value)
-                }
-                sx={style}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-              />
-
-              <TextField
-                label="Date of Birth"
-                value={client.dateOfBirth || ""}
-                onChange={(e) =>
-                  handleInputChange("dateOfBirth", e.target.value)
-                }
-                placeholder="MM/DD/YYYY"
-                sx={{ background: "#292929", borderRadius: 1 }}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-              />
-
-              <FormControl sx={{ margin: "0 auto" }}>
-                <Typography
-                  variant="body1"
-                  sx={{ mb: 2, marginLeft: 10, fontSize: 25 }}
-                >
-                  Sex
-                </Typography>
-                <RadioGroup
-                  row
-                  value={client.gender || ""}
-                  onChange={(e) => handleInputChange("gender", e.target.value)}
-                  sx={{ display: "flex", gap: "2rem" }}
-                >
-                  <FormControlLabel
-                    value="Male"
-                    control={<PanoramaWideAngleSelectIcon />}
-                    label="Male"
-                    sx={{ color: "#CBCBCB", marginLeft: "1.5rem" }}
-                  />
-                  <FormControlLabel
-                    value="Female"
-                    control={<PanoramaWideAngleSelectIcon />}
-                    label="Female"
-                    sx={{ color: "#CBCBCB" }}
-                  />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-          </Grid2>
-
-          <Grid2
-            item
-            xs={12}
-            md={6}
-            sx={{ display: "flex", flexDirection: "column", gap: 5 }}
-          >
-            <Typography
-              variant="h5"
-              sx={{ mt: 6, fontSize: 50, margin: "0 auto", fontWeight: "bold" }}
-            >
-              Contact Info
-            </Typography>
-            <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-              <TextField
-                label="Email"
-                value={client.email || ""}
-                onChange={(e) => handleInputChange("email", e.target.value)}
-                sx={style}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-              />
-              <TextField
-                label="Phone"
-                value={client.phone || ""}
-                onChange={(e) => handleInputChange("phone", e.target.value)}
-                sx={style}
-                InputLabelProps={{
-                  style: { color: "white" },
-                }}
-              />
               <Typography
                 variant="h5"
                 sx={{
                   mb: 2,
                   fontSize: 50,
-                  fontWeight: "bold",
                   margin: "0 auto",
-                  marginTop: 3,
+                  fontWeight: "bold",
                 }}
               >
-                Social Links
+                User Info
               </Typography>
-              <TextField
-                label="VK"
-                value={client.vk || ""}
-                onChange={(e) => handleInputChange("vk", e.target.value)}
-                sx={style}
-                InputLabelProps={{
-                  style: { color: "white" },
+              <Box
+                sx={{
+                  width: 400,
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 5,
+                  color: "white",
                 }}
-              />
-            </Box>
-          </Grid2>
-        </Grid2>
+              >
+                <TextField
+                  label="Фамилия"
+                  value={client.surname}
+                  onChange={(e) => handleInputChange("surname", e.target.value)}
+                  sx={{
+                    
+                    "& .MuiInputBase-root": {
+                      color: "white", 
+                    },
+                    "& .MuiInputLabel-root": {
+                      color: "white", 
+                    },
+                    
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white"
+                    },
+                  }}
+                />
 
-        {/* Action Buttons */}
-        <Box sx={{ display: "flex", gap: 1, mt: 4 }}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleSave}
-            sx={{
-              background: "#303030",
-              height: 90,
-              fontSize: 24,
-              borderRadius: 5,
-              padding: "0 7rem 0 7rem",
-            }}
-          >
-            Save changes
-          </Button>
-          <Button
-            variant="contained"
-            color="error"
-            onClick={handleDelete}
-            sx={{
-              background: "#611B1B",
-              fontSize: 16,
-              borderRadius: 10,
-              height: 50,
-              marginTop: 3.5,
-              padding: "2rem",
-            }}
-          >
-            Remove User
-          </Button>
-          <Dialog
-            sx={{
-              "& .MuiDialog-paper": {
-                backgroundColor: "#1B1B1B",
-                width: "424px",
-                height: "274px",
-                borderRadius: "30px",
-              },
-            }}
-            open={showConfirm}
-            onClose={cancelDelete}
-          >
-            <DialogTitle
+                <TextField
+                  label="Имя"
+                  value={client.name}
+                  onChange={(e) => handleInputChange("name", e.target.value)}
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      color: "white",
+                    },
+                    "& .MuiInputLabel-root": {
+                      color: "white", 
+                    },
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white"
+                    },
+                  }}
+                />
+
+                <TextField
+                  label="Отчество"
+                  value={client.lastName || ""}
+                  onChange={(e) =>
+                    handleInputChange("lastName", e.target.value)
+                  }
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      color: "white", 
+                    },
+                    "& .MuiInputLabel-root": {
+                      color: "white", 
+                    },
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white"
+                    },
+                  }}
+                />
+              </Box>
+            </Grid2>
+
+            <Grid2
+              item
+              xs={12}
+              md={6}
+              sx={{ display: "flex", flexDirection: "column", gap: 5 }}
+            >
+              <Typography
+                variant="h5"
+                sx={{
+                  mt: 6,
+                  fontSize: 50,
+                  margin: "0 auto",
+                  fontWeight: "bold",
+                }}
+              >
+                Contact Info
+              </Typography>
+              <Box sx={{ display: "flex", flexDirection: "column", gap: 2, width: 400 }}>
+                <TextField
+                  label="Email"
+                  value={client.email || ""}
+                  onChange={(e) => handleInputChange("email", e.target.value)}
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      color: "white", 
+                    },
+                    "& .MuiInputLabel-root": {
+                      color: "white",
+                    },
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white"
+                    },
+                  }}
+                />
+                <TextField
+                  label="Phone"
+                  value={client.contacts[1].value || ""}
+                  onChange={(e) => handleInputChange("phone", e.target.value)}
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      color: "white", 
+                    },
+                    "& .MuiInputLabel-root": {
+                      color: "white",
+                    },
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white"
+                    },
+                  }}
+                />
+                <Typography
+                  variant="h5"
+                  sx={{
+                    mb: 2,
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    margin: "0 auto",
+                    marginTop: 3,
+                  }}
+                >
+                  Social Links
+                </Typography>
+                <TextField
+                  label="VK"
+                  value={client.vk || ""}
+                  onChange={(e) => handleInputChange("vk", e.target.value)}
+                  sx={{
+                    "& .MuiInputBase-root": {
+                      color: "white", 
+                    },
+                    "& .MuiInputLabel-root": {
+                      color: "white",
+                    },
+                    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                      borderColor: "white"
+                    },
+                  }} 
+                />
+              </Box>
+            </Grid2>
+          </Grid2>
+
+          
+          <Box sx={{ display: "flex", gap: 1, mt: 4 }}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleSave}
               sx={{
-                color: "white",
-                textAlign: "center",
-                marginTop: "25px",
-                fontSize: "18px",
+                background: "#303030",
+                height: 90,
+                fontSize: 24,
+                borderRadius: 5,
+                padding: "0 7rem 0 7rem",
               }}
             >
-              Удалить клиента
-            </DialogTitle>
-            <DialogContent
+              Save changes
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={handleDelete}
               sx={{
-                color: "white",
-                textAlign: "center",
-                fontSize: "14px",
-                maxWidth: "300px",
-                marginLeft: "auto",
-                marginRight: "auto",
+                background: "#611B1B",
+                fontSize: 16,
+                borderRadius: 10,
+                height: 50,
+                marginTop: 3.5,
+                padding: "2rem",
               }}
             >
-              Вы действительно хотите удалить данного клиента?
-            </DialogContent>
-            <DialogActions
+              Remove User
+            </Button>
+            <Dialog
               sx={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                gap: 2,
+                "& .MuiDialog-paper": {
+                  backgroundColor: "#1B1B1B",
+                  width: "424px",
+                  height: "274px",
+                  borderRadius: "30px",
+                },
               }}
+              open={showConfirm}
+              onClose={cancelDelete}
             >
-              <Button
-                onClick={confirmDelete}
-                color="error"
+              <DialogTitle
                 sx={{
                   color: "white",
-                  borderRadius: "13px",
-                  padding: "13px",
-                  width: "150px",
-                  height: "40px",
-                  backgroundColor: "#555555",
-                  fontSize: "14px",
-                  textTransform: "none",
+                  textAlign: "center",
+                  marginTop: "25px",
+                  fontSize: "18px",
                 }}
               >
-                Удалить
-              </Button>
-              <Button
-                onClick={cancelDelete}
-                color="primary"
+                Удалить клиента
+              </DialogTitle>
+              <DialogContent
                 sx={{
-                  color: "#8F8F8F",
-                  fontSize: "12px",
-                  textTransform: "none",
+                  color: "white",
+                  textAlign: "center",
+                  fontSize: "14px",
+                  maxWidth: "300px",
+                  marginLeft: "auto",
+                  marginRight: "auto",
                 }}
               >
-                Отмена
-              </Button>
-            </DialogActions>
-          </Dialog>
+                Вы действительно хотите удалить данного клиента?
+              </DialogContent>
+              <DialogActions
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  gap: 2,
+                }}
+              >
+                <Button
+                  onClick={confirmDelete}
+                  color="error"
+                  sx={{
+                    color: "white",
+                    borderRadius: "13px",
+                    padding: "13px",
+                    width: "150px",
+                    height: "40px",
+                    backgroundColor: "#555555",
+                    fontSize: "14px",
+                    textTransform: "none",
+                  }}
+                >
+                  Удалить
+                </Button>
+                <Button
+                  onClick={cancelDelete}
+                  color="primary"
+                  sx={{
+                    color: "#8F8F8F",
+                    fontSize: "12px",
+                    textTransform: "none",
+                  }}
+                >
+                  Отмена
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </>
   );
 }
